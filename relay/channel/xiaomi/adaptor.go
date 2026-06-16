dab355070b870d3021fbe59d097291692c435fa1
hetao_awa <3112871060@qq.com>
2026-06-16 16:20:18 +0800

    fix(xiaomi): update endpoint for chat completions in adaptor

--- relay/channel/xiaomi/adaptor.go
+++ relay/channel/xiaomi/adaptor.go
@@ -51,7 +51,7 @@ func (a *Adaptor) GetRequestURL(info *relaycommon.RelayInfo) (string, error) {
 			case relayconstant.RelayModeEmbeddings:
 				return fmt.Sprintf("%s/embeddings", specialPlan.OpenAIBaseURL), nil
 			case relayconstant.RelayModeResponses:
-				return fmt.Sprintf("%s/responses", specialPlan.OpenAIBaseURL), nil
+				return fmt.Sprintf("%s/chat/completions", specialPlan.OpenAIBaseURL), nil
 			case relayconstant.RelayModeResponsesCompact:
 				return fmt.Sprintf("%s/responses/compact", specialPlan.OpenAIBaseURL), nil
 			default:
